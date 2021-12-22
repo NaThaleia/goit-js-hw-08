860289fcb8f62199f777e771478fe6a086c658f4
NaThaleia <miss.mekh@gmail.com>
2021-12-22 18:37:52 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCards(galleryItems);
+
+galleryContainer.innerHTML = cardsMarkup;
+
+// settings
+const lightboxSettings = {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+function createGalleryCards(gallery) {
+  return gallery
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    })
+    .join('');
+}
+
+const createGallery = new SimpleLightbox('.gallery a', lightboxSettings);
