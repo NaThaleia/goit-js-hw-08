ae34c257f4af9183b2d175c555d9850b46c66437
NaThaleia <miss.mekh@gmail.com>
2021-12-22 19:20:35 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+// Инициализируем плеер в файле скрипта согласно документации
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
 
+// берем лодеш
+const throttle = require('lodash.throttle');
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', Math.round(data.seconds));
+  console.log(Number(localStorage.getItem('videoplayer-current-time')));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const onSetup = function () {
+  player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+};
+window.onload = onSetup;
